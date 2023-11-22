c367a1b03ebebafac4a9b178f3d5889a7fafa603
Alec Hinh <alec4555@gmail.com>
2023-11-22 14:51:48 -0600

    Update olm

--- operator-lifecycle-manager/install-olm.sh
+++ operator-lifecycle-manager/install-olm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-olm_version="v0.24.0"
+olm_version="v0.26.0"
 
 curl -L https://github.com/operator-framework/operator-lifecycle-manager/releases/download/$olm_version/install.sh -o install.sh
 chmod +x install.sh
