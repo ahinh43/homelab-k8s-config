d45b166c2d4133461100f12c18d51b0adba52d9e
Alec Hinh <alec4555@gmail.com>
2023-11-22 00:01:57 -0600

    Update cert manager and move gateway to nginx-gateway

--- cert-manager/install-crds.sh
+++ cert-manager/install-crds.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-CRDVERSION="v1.11.0"
+CRDVERSION="v1.13.2"
 
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/${CRDVERSION}/cert-manager.crds.yaml
