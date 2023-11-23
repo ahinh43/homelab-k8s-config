7de3cf8820a2614aa8a30cce9b12d509c5f10a2a
Alec Hinh <alec4555@gmail.com>
2023-11-22 21:29:23 -0600

    Ensure components are deployed to all clusters except argo

--- provision.sh
+++ provision.sh
@@ -13,10 +13,6 @@ else
   exit $?
 fi
 
-# Cloudflare API token secret needed for cert-manager to auto complete the domain verification
-kubectl create secret generic cloudflare-api-token --from-literal=api_token=$(op item get "Cloudflare API Token" --vault Homelab --fields credential) --context kube
-kubectl create secret generic cloudflare-api-token --from-literal=api_token=$(op item get "Cloudflare API Token" --vault Homelab --fields credential) --context kube2
-
 kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml --context kube
 kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml --context kube2
 
@@ -25,7 +21,13 @@ kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/
 
 echo "Deploying the necessary services to get the cluster rolling..."
 
-helm install -f cert-manager/values.yaml --namespace cert-manager --create-namespace cert-manager-kube1 cert-manager/
+helm install -f cert-manager/values.yaml --kube-context kube --namespace cert-manager --create-namespace cert-manager-kube1 cert-manager/
+helm install -f cert-manager/values.yaml --kube-context kube2 --namespace cert-manager --create-namespace cert-manager-kube2 cert-manager/
+
+# Cloudflare API token secret needed for cert-manager to auto complete the domain verification
+kubectl create secret generic cloudflare-api-token --namespace cert-manager --from-literal=api_token=$(op item get "Cloudflare API Token" --vault Homelab --fields credential) --context kube
+kubectl create secret generic cloudflare-api-token --namespace cert-manager --from-literal=api_token=$(op item get "Cloudflare API Token" --vault Homelab --fields credential) --context kube2
+
 
 kubectl apply -f cert-manager/le-prod-clusterissuer.yaml --context kube
 kubectl apply -f cert-manager/le-prod-clusterissuer.yaml --context kube2
@@ -33,8 +35,11 @@ kubectl apply -f cert-manager/le-prod-clusterissuer.yaml --context kube2
 kubectl apply -f cert-manager/le-staging-clusterissuer.yaml --context kube
 kubectl apply -f cert-manager/le-staging-clusterissuer.yaml --context kube2
 
-helm install --namespace nginx-gateway --create-namespace nginx-gateway-fabric-kube1 nginx-gateway-fabric/
-kubectl apply -f nginx-gateway-fabric/gateway.yaml
+helm install --kube-context kube --namespace nginx-gateway --create-namespace nginx-gateway-fabric-kube1 nginx-gateway-fabric/
+helm install --kube-context kube2 --namespace nginx-gateway --create-namespace nginx-gateway-fabric-kube2 nginx-gateway-fabric/
+kubectl apply -f nginx-gateway-fabric/gateway.yaml --context kube
+kubectl apply -f nginx-gateway-fabric/gateway.yaml --context kube2
+
 
 helm install -f argocd/values.yaml --namespace argocd --create-namespace argocd-kube1 argocd/
 kubectl apply -f argocd/http-route.yaml
