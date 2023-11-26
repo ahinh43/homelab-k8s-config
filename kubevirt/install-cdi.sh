3656557575e5df02e0cf492f1ee090f812f4d6f9
Alec Hinh <alec4555@gmail.com>
2023-11-26 13:41:54 -0600

    Add CDI install scripts

--- kubevirt/install-cdi.sh
+++ kubevirt/install-cdi.sh
@@ -32,3 +32,8 @@ metadata:
 data:
   feature-gates: "LiveMigration"
 EOF
+
+export TAG=$(curl -s -w %{redirect_url} https://github.com/kubevirt/containerized-data-importer/releases/latest)
+export VERSION=$(echo ${TAG##*/})
+kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-operator.yaml
+kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-cr.yaml
