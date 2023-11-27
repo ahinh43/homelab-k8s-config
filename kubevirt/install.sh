9e507cf49c0cb813260c2574321830ea779bd858
Alec Hinh <alec4555@gmail.com>
2023-11-27 14:01:23 -0600

    Consolidate kubevirt install script (2)

--- kubevirt/install.sh
+++ kubevirt/install.sh
@@ -6,6 +6,11 @@
 
 set -euxo pipefail
 
+export TAG=$(curl -s -w %{redirect_url} https://github.com/kubevirt/containerized-data-importer/releases/latest)
+export VERSION=$(echo ${TAG##*/})
+kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-operator.yaml
+kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-cr.yaml
+
 # Point at latest release
 export RELEASE=$(curl https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirt/stable.txt)
 # Deploy the KubeVirt operator
