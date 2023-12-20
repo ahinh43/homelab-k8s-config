62dd9eb93dc508dd01f0de55b1b7f8ed5b60132c
Alec Hinh <alec4555@gmail.com>
2023-12-19 18:48:37 -0600

    Remove oLM

--- operator-lifecycle-manager/install-olm.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-olm_version="v0.26.0"
-
-curl -L https://github.com/operator-framework/operator-lifecycle-manager/releases/download/$olm_version/install.sh -o install.sh
-chmod +x install.sh
-./install.sh $olm_version
-
-rm install.sh
