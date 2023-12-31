d620ab15835f1dba6d9ca5c2e319e6438e859a1c
Alec Hinh <alec4555@gmail.com>
2023-12-31 10:58:42 -0600

    Add profile and tribe backup

--- kubevirt/vms/overlays/asa-ds-1/backupArk.sh
+++ kubevirt/vms/overlays/asa-ds-1/backupArk.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 dirname=$(date +%Y-%m-%d-%H-%M-%S-arkbackup)
-basepath="/home/steam/.local/share/Steam/steamapps/common/ARK Survival Ascended Dedicated Server/ShooterGame/Saved"
+basepath="/home/steam/ark/Saved"
 
+# Backup server config
 aws --endpoint-url http://minio.labs.ahinh.me:9000 s3 cp --recursive "${basepath}/Config" s3://ark-sa/$dirname/Config --exclude '.git/*' --exclude 'CrashReportClient/*'
+# Backup mod configurations
 aws --endpoint-url http://minio.labs.ahinh.me:9000 s3 cp --recursive "${basepath}/SaveGames" s3://ark-sa/$dirname/SaveGames
+# Backup tribe profiles and characters
+aws --endpoint-url http://minio.labs.ahinh.me:9000 s3 cp --recursive "${basepath}/SavedArks/TheIsland_WP" s3://ark-sa/$dirname/TheIsland_WP --include '*.arkprofile' --include '*.profilebak' --include '*.arktribe' --include '*.tribebak' --exclude '*.ark' --exclude '*.bak'
+# Backup the world
 aws --endpoint-url http://minio.labs.ahinh.me:9000 s3 cp "${basepath}/SavedArks/TheIsland_WP/TheIsland_WP.ark" s3://ark-sa/$dirname/TheIsland_WP/TheIsland_WP.ark
+# Backup the backup of the world
 aws --endpoint-url http://minio.labs.ahinh.me:9000 s3 cp "${basepath}/SavedArks/TheIsland_WP/TheIsland_WP_AntiCorruptionBackup.bak" s3://ark-sa/$dirname/TheIsland_WP/TheIsland_WP_AntiCorruptionBackup.bak
-
-# Backup ark profiles
-
-for file in $(ls *.arkprofile *.profilebak *.arktribe *.tribebak); do 
-  aws --endpoint-url http://minio.labs.ahinh.me:9000 s3 cp "${basepath}/SavedArks/TheIsland_WP/$file" s3://ark-sa/$dirname/TheIsland_WP/$file; 
-done
