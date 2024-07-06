3e32bcac6f5b93143e82b907294d81544f772672
Alec Hinh <alec4555@gmail.com>
2024-07-06 12:27:59 -0500

    Fix the backup script
    Signed-off-by: Alec Hinh <alec4555@gmail.com>

--- kubevirt/vms/overlays/terraria-1/backupTerraria.sh
+++ kubevirt/vms/overlays/terraria-1/backupTerraria.sh
@@ -4,5 +4,4 @@ dirname=$(date +%Y-%m-%d-%H-%M-%S-terraria-backup)
 basepath="/home/steam/.local/share/Terraria/tModLoader/Worlds"
 
 # Backup the world (but not the bak)
-aws --endpoint-url http://minio.labs.ahinh.me:9000 s3 cp --recursive "${basepath}/????.twld" s3://terraria/$dirname/????.twld
-aws --endpoint-url http://minio.labs.ahinh.me:9000 s3 cp --recursive "${basepath}/????.wld" s3://terraria/$dirname/????.wld
+aws --endpoint-url http://minio.labs.ahinh.me:9000 s3 cp --recursive "${basepath}/Worlds" s3://terraria/$dirname/Worlds --exclude 'Backups/*'
