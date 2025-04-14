9751e64e04fe70d6b3f7106ed9a5de87582bc5e9
Alec Hinh <alec4555@gmail.com>
2025-04-13 22:20:49 -0500

    Remove VMs
    Signed-off-by: Alec Hinh <alec4555@gmail.com>

--- kubevirt/vms/overlays/palworld-2/backupPal.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-dirname=$(date +%Y-%m-%d-%H-%M-%S-palbackup)
-basepath="/home/steam/palworld/Pal/Saved"
-
-# Backup server config
-aws --endpoint-url http://minio.labs.ahinh.me:9000 s3 cp --recursive "${basepath}/Config" s3://palworld/$dirname/Config --exclude '.git/*' --exclude 'CrashReportClient/*'
-# Backup the world and players
-aws --endpoint-url http://minio.labs.ahinh.me:9000 s3 cp --recursive "${basepath}/SaveGames/0/95A2B2D0ABE34D79B9E02C780546E012" s3://palworld/$dirname/SaveGames/0/95A2B2D0ABE34D79B9E02C780546E012
