096e2443ba3f0602ae3f92f1d66297b3569fceb7
Alec Hinh <alec4555@gmail.com>
2025-11-15 10:43:05 -0600

    Get rid of kubevirt
    Signed-off-by: Alec Hinh <alec4555@gmail.com>

--- kubevirt/vms/overlays/terraria-1/backupTerraria.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-dirname=$(date +%Y-%m-%d-%H-%M-%S-terraria-backup)
-basepath="/home/steam/.local/share/Terraria/tModLoader/Worlds"
-
-# Backup the world (but not the bak)
-aws --endpoint-url http://minio.labs.ahinh.me:9000 s3 cp --recursive "${basepath}" s3://terraria/$dirname/Worlds --exclude 'Backups/*'
